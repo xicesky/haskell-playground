8e5dcd70f670c536c4d74ab5bc9aea5ab2b64020
Markus Dangl <663399+xicesky@users.noreply.github.com>
2021-03-23 21:00:40 +0100

    FusedEffects use actual effects.

--- nondet/src/NonDetSearch/FusedEffects.hs
+++ nondet/src/NonDetSearch/FusedEffects.hs
@@ -2,11 +2,13 @@
 -- Source: https://hackage.haskell.org/package/fused-effects-1.1.1.0/docs/Control-Effect-NonDet.html
 module NonDetSearch.FusedEffects where
 
-import Data.Functor.Identity
-import Control.Effect.NonDet
+import Data.Functor.Identity (Identity)
+import Control.Effect.Empty (empty)
+import Control.Effect.Choose ((<|>))
+import Control.Effect.NonDet (oneOf)
 -- import Control.Effect.Cut
 -- import Control.Effect.Cull
-import Control.Carrier.NonDet.Church
+import Control.Carrier.NonDet.Church (NonDetC, run, runNonDetA)
 
 import qualified NonDet.Class as NDC
 
@@ -16,7 +18,9 @@ type Carrier = NonDetC Identity
 -- NonDet interface
 
 instance NDC.NonDet Carrier where
-    -- anyof = aMemberOf
+    failure = empty
+    choice = (<|>)
+    anyof = oneOf
 
 searchFE :: NDC.SFun
 searchFE = NDC.SFun search where
